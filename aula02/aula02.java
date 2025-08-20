c44ca9bdff737d37314f86974444f83f20436f35
Layo Mendes <165064021+LayoMendes@users.noreply.github.com>
2025-08-20 00:02:56 +0000

    Aula banco de dados

---
+++ aula02/aula02.java
@@ -0,0 +1,20 @@
+class No<t>{
+    private T dado;
+    private No<t> nextNo;
+
+    public No(T dado){
+        this(dado, null);
+
+    }
+    public No(T dado, No<t> no){
+        this.dado = dado;
+        this.nextNo = no;
+    }
+    public T getDado(){
+        return this.dado;
+    }
+
+    public void setDado(T dado){
+        this.dado = dado;
+    }
+}
