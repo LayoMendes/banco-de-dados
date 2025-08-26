a0b7a9b8464d94c5ca2bb67c269c4905b8493be2
Layo Mendes <165064021+LayoMendes@users.noreply.github.com>
2025-08-26 23:25:03 +0000

    banco de dados atualização

--- aula03/schema.sql/schema.sql
+++ aula03/schema.sql/schema.sql
@@ -9,3 +9,45 @@ CREATE TABLE usuario(
     email TEXT NOT NULL,
     senha INTEGER NOT NULL
 );
+
+-- tabela cliente 
+
+CREATE TABLE cliente(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    telefone TEXT NOT NULL,
+    usuario_id INTEGER NOT NULL UNIQUE,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE
+);
+
+--TABELA PRODUTO
+CREATE TABLE produto(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    descricao TEXT,
+    preco REAL NOT NULL CHECK (preco >= 0)
+);
+
+CREATE TABLE venda(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    data TEXT NOT NULL,
+    usuario_id INTEGER NOT NULL,
+    cliente_id INTEGER NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE SET NULL,
+    FOREIGN KEY (cliente_id) REFERENCES cliente(id) ON DELETE SET NULL
+
+    );
+
+-- TABELA VENDA_PRODUT tabela associativa para N:M produto <-> venda
+
+CREATE TABLE venda_produto(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    venda_id INTEGER NOT NULL,
+    produto_id INTEGER NOT NULL,
+    quantidade INTEGER NOT NULL CHECK (quantidade > 0),
+    preco_unitario REAL NOT NULL CHECK (preco_unitario >= 0),
+   -- PRIMARY KEY (venda_id, produto_id), 
+    FOREIGN KEY (venda_id) REFERENCES venda(id) ON DELETE CASCADE,
+    FOREIGN KEY (produto_id) REFERENCES produto(id) ON DELETE CASCADE,
+
+ )
