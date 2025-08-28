b7b2112cef0333968db8daf6ee352ad182df2ac5
Layo Mendes <165064021+LayoMendes@users.noreply.github.com>
2025-08-28 00:33:15 +0000

    Banco de Dados 27/08

--- aula03/schema.sql/schema.sql
+++ aula03/schema.sql/schema.sql
@@ -51,3 +51,20 @@ CREATE TABLE venda_produto(
     FOREIGN KEY (produto_id) REFERENCES produto(id) ON DELETE CASCADE,
 
  )
+
+INSERT INTO usuario (nome, email, senha) VALUES ('Rafael', 'rafael@unipam', 'leafar');
+INSERT INTO usuario (nome, email, senha) VALUES ('Layo', 'layo@unipam', 'leafar');
+INSERT INTO usuario (nome, email, senha) VALUES ('Ana Clara Silva Dias', 'muiezinha@unipam', 'leafar');
+INSERT INTO usuario (nome, email, senha) VALUES ('pretinha', 'pretinha@unipam', 'leafar');
+
+
+INSERT INTO cliente (nome, telefone, usuario_id) VALUES ('Rafael Marinho', '999999', 1);
+INSERT INTO cliente (nome, telefone, usuario_id) VALUES ('Layo Mendes', '999999', 2);
+INSERT INTO cliente (nome, telefone, usuario_id) VALUES ('Ana Clara', '999999', 3);
+INSERT INTO cliente (nome, telefone, usuario_id) VALUES ('Pret', '999999', 4);
+
+
+INSERT INTO produto (nome, descricao, preco) VALUES ('Arroz 5kg', 'tipo bomzinho', 70.00);
+INSERT INTO produto (nome, descricao, preco) VALUES ('Aptamil Premium', '800g', 110.00);
+INSERT INTO produto (nome, descricao, preco) VALUES ('Neocate', '400g', 188.50);
+INSERT INTO produto (nome, descricao, preco) VALUES ('Fortini Plus', '400g', 110.00);
